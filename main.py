826495bc76b3b39263f5f19f6f7e2556d5be969c
Daniele Golzio <80271815+poonchoi@users.noreply.github.com>
2022-01-20 14:13:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-import curses
-from curses import wrapper
-from curses.textpad import Textbox, rectangle
-import time
+'''
+curses experimenting
+'''
+# import curses
+# from curses import wrapper
+# from curses.textpad import Textbox, rectangle
+# import time
 
-stdscr = initstdscr()
-curses.curs_set(0)
+# stdscr = initstdscr()
+# curses.curs_set(0)
 
-def main(stdscr):
-    # Clear screen
-    stdscr.clear()
+# def main(stdscr):
+#     # Clear screen
+#     stdscr.clear()
 
-    rectangle(stdscr, 2, 2, 10, 20)
-    stdscr.refresh()
-    stdscr.getkey()
+#     rectangle(stdscr, 2, 2, 10, 20)
+#     stdscr.refresh()
+#     stdscr.getkey()
 
-wrapper(main)
+# wrapper(main)
+
+'''
+Typer experimenting
+'''
+import typer
+
+
+def main(username: str):
+    if username == "root":
+        typer.echo("The root user is reserved")
+        raise typer.Abort()
+    typer.echo(f"New user created: {username}")
+
+
+if __name__ == "__main__":
+    typer.run(main)
+
+
