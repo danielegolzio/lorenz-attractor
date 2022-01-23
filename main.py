bca53c3f56fcb75dad926e0040841ba95254142c
dan <80271815+poonchoi@users.noreply.github.com>
2022-01-23 16:46:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-<<<<<<< Updated upstream
-'''
-curses experimenting
-'''
-# import curses
-# from curses import wrapper
-# from curses.textpad import Textbox, rectangle
-# import time
-
-# stdscr = initstdscr()
-# curses.curs_set(0)
-
-# def main(stdscr):
-#     # Clear screen
-#     stdscr.clear()
-
-#     rectangle(stdscr, 2, 2, 10, 20)
-#     stdscr.refresh()
-#     stdscr.getkey()
-
-# wrapper(main)
-
 '''
 Typer experimenting
 '''
@@ -35,38 +13,3 @@ def main(username: str):
 
 if __name__ == "__main__":
     typer.run(main)
-
-
-=======
-import curses
-from curses import wrapper
-
-
-fish = """
-           FISHKISSFISHKIS               
-       SFISHKISSFISHKISSFISH            F
-    ISHK   ISSFISHKISSFISHKISS         FI
-  SHKISS   FISHKISSFISHKISSFISS       FIS
-HKISSFISHKISSFISHKISSFISHKISSFISH    KISS
-  FISHKISSFISHKISSFISHKISSFISHKISS  FISHK
-      SSFISHKISSFISHKISSFISHKISSFISHKISSF
-  ISHKISSFISHKISSFISHKISSFISHKISSF  ISHKI
-SSFISHKISSFISHKISSFISHKISSFISHKIS    SFIS
-  HKISSFISHKISSFISHKISSFISHKISS       FIS
-    HKISSFISHKISSFISHKISSFISHK         IS
-       SFISHKISSFISHKISSFISH            K
-         ISSFISHKISSFISHK               
-      """
-
-def main(stdscr):
-    stdscr.clear()
-    fish = curses.newwin(10,10)
-    fish.addstr("hello world")
-    fish.refresh()
-    for i in range(12):
-        fish.move(0,1)
-        stdscr.refresh()
-        stdscr.clear()
-    stdscr.getch()
-wrapper(main)
->>>>>>> Stashed changes
