aae393b2260efd9b8ca6ebe46261a90f4306204c
dan <80271815+poonchoi@users.noreply.github.com>
2022-01-23 22:10:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,18 @@
 Typer experimenting
 '''
 import typer
-
+import os 
+import time
 
 def main(username: str):
+    """
+    yo
+    """
     if username == "root":
         typer.echo("The root user is reserved")
         raise typer.Abort()
-    typer.echo(f"New user created: {username}")
+    typer.secho(f"New user created: {username}", fg=typer.colors.MAGENTA, bg=typer.colors.RED)
+
 
 
 if __name__ == "__main__":
