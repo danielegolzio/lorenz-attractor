d2f9c25cd3fa315017c483319db50f9ce32dcb74
dan <80271815+poonchoi@users.noreply.github.com>
2022-02-03 22:59:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from tkinter import N
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
+import math as m
+
 
 def collatz_conjecture(n):
 
@@ -23,4 +26,43 @@ def collatz_conjecture(n):
         plt.show()
         plt.figure().clear()
 
-collatz_conjecture(12345)
+def chaos():
+    """
+    Xn+1 = RXn (1-Xn)
+    """
+    ax = plt.subplots()
+    xp = []
+    yp = []
+
+    n = 0
+    r = 1
+
+    while n != 1:
+        x = n
+        x = r * x * (1 - x)
+        yp.append(x)
+        xp.append(n)
+        n += 0
+    
+    plt.plot(xp, yp)
+    plt.show()
+
+def circle():
+    ax = plt.subplots()
+    xp = []
+    yp = []
+
+    t = 0
+    y = 0
+    x = 0
+    
+    while t != 100:
+        x = m.sin(t)
+        y = m.cos(t)
+        yp.append(y)
+        xp.append(x)
+        t+=0.1
+    plt.plot(xp, yp)
+    plt.show()
+
+circle()
