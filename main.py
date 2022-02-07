c4547ab4d930b1e133031b59ecdaac6844dc2e7e
dan <80271815+poonchoi@users.noreply.github.com>
2022-02-08 00:00:00 +0100

    FEAT: added colors

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import numpy as np
 import pygame
 import typer
+from colors import colors
 
-def lorenz():
+def main(
+    color: str = typer.Argument(None)
+):
+
+    if color == "r":
+        colorI = 0
+    elif color == "o":
+        colorI = 1
+    elif color == "y":
+        colorI = 2
+    elif color == "g":
+        colorI = 3
+    elif color == "b":
+        colorI = 4
+    elif color == "i":
+        colorI = 5
+    elif color == "v":
+        colorI = 6
+    else:
+        colorI = 7
 
     pygame.init()
     dimensions = (1920, 1080)
@@ -11,7 +31,6 @@ def lorenz():
     screen = pygame.display.set_mode(dimensions)
     clock = pygame.time.Clock()
     fps = 75
-    white = (255,255,255)
 
     o = 10.0 # sigma
     p = 28.0 # rho
@@ -19,8 +38,7 @@ def lorenz():
     x = 0.01
     y = 0
     z = 0
-    scale = 16
-    t = 0
+    scale = 15
 
     run = True
     screen.fill((0,0,0))
@@ -29,7 +47,7 @@ def lorenz():
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
                 run = False
-        
+
         prev_coords = (int((x*scale)+500+width//2),int((y*scale)+height//2)+height//2)
 
         t = 0.01
@@ -44,9 +62,10 @@ def lorenz():
         
         coords = (int((x*scale)+500+width//2),int((y*scale)+height//2)+height//2)
         
-        pygame.draw.line(screen, white, prev_coords, coords)
+        pygame.draw.line(screen, colors[colorI], prev_coords, coords)
         pygame.display.update()
+
 pygame.quit()
 
 if __name__ == "__main__":
-    lorenz()
+    typer.run(main)
