dee5fa7e2f0611fd7418aa0e9f1bf93b2d1fcc82
dan <80271815+poonchoi@users.noreply.github.com>
2022-02-12 21:28:51 +0100

    FEAT: program now has multiple particles

--- main.py
+++ main.py
@@ -1,51 +1,73 @@
-import numpy as np
 import pygame
+import random
 import typer
-from colors import colors
 
 def main(
+    speed: int = typer.Argument(None),
     version: str = typer.Argument(None),
-    color: str = typer.Argument(None),
-    speed: int = typer.Argument(None)
+    population: int = typer.Argument(None),
+    size: int = typer.Argument(None)
 ):
-
-    if color == "r":
-        colorI = 0
-    elif color == "o":
-        colorI = 1
-    elif color == "y":
-        colorI = 2
-    elif color == "g":
-        colorI = 3
-    elif color == "b":
-        colorI = 4
-    elif color == "i":
-        colorI = 5
-    elif color == "v":
-        colorI = 6
-    else:
-        colorI = 7
-
+    # pygame initialize
     pygame.init()
 
-    dimensions = (1920, 1080)
+    dimensions = (1920,1080)
     width = dimensions[0]//2
     height = dimensions[1]//2
 
     screen = pygame.display.set_mode(dimensions)
     clock = pygame.time.Clock()
 
+    screen.fill((0,0,0))
+
+    if size != None:
+        size = size
+    else:
+        size = 1
+
+    # constants
+    o = 10.0 # sigma
+    rho = 28.0 # rho
+    B = 8.0 / 3.0 # beta
+
+    # particle class
+    class particle():
+        def __init__(self, t, x, y, z, color, scale, version):
+            self.t = t
+            self.x = x
+            self.y = y
+            self.z = z
+            self.color = color
+            self.scale = scale
+            self.version = version
+
+        def calc(self):
+            dx = (o * (self.y - self.x)) * self.t
+            self.x = self.x + dx
+
+            dy = ((self.x * (rho - self.z)) - self.y) * self.t
+            self.y = self.y + dy
+
+            dz = ((self.x * self.y) - (B * self.z)) * self.t
+            self.z = self.z + dz
+
+        def draw(self):
+            if self.version == "xy" or self.version == "yx":
+                coords = (int((self.x*self.scale)+500+width//2),int((self.y*self.scale)+height//2)+height//2)
+            elif self.version == "zy" or self.version == "yz":
+                coords = (int((self.z*self.scale)+80+width//2),int((self.y*self.scale)+height//2)+height//2)
+            elif self.version == "xz" or self.version == "zx":
+                coords = (int((self.x*self.scale)+500+width//2),int((self.z*self.scale)+height//4))
+            else:
+                coords = (int((self.x*self.scale)+500+width//2),int((self.y*self.scale)+height//2)+height//2)
+
+            pygame.draw.circle(screen, self.color, coords, size)
+            pygame.display.update()
+
     if speed != None:
         fps = speed
     else:
         fps = 60
-
-    o = 10.0 # sigma
-    p = 28.0 # rho
-    B = 8.0 / 3.0 # beta
-    x = 0.01
-    y = 0
-    z = 0
 
     if version == "xy" or version == "yx":
         scale = 15
@@ -55,47 +77,24 @@ def main(
         scale = 17
     else:
         scale = 15
+    
+    if population != None:
+        population = population
+    else:
+        population = 1
+
+    p = [particle((0.01+(i*(0.01/population))), 0.01, 0, 0, (random.randint(50,255),random.randint(50,255),random.randint(50,255)), scale, version) for i in range(population)]
 
     run = True
-    screen.fill((0,0,0))
     while run:
+        screen.fill((0,0,0))
         clock.tick(fps)
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
                 run = False
-        if version == "xy" or version == "yx":
-            prev_coords = (int((x*scale)+500+width//2),int((y*scale)+height//2)+height//2)
-        elif version == "zy" or version == "yz":
-            prev_coords = (int((z*scale)+80+width//2),int((y*scale)+height//2)+height//2)
-        elif version == "xz" or version == "zx":
-            prev_coords = (int((x*scale)+500+width//2),int((z*scale)+height//4))
-        else:
-            prev_coords = (int((x*scale)+500+width//2),int((y*scale)+height//2)+height//2)
-
-        t = 0.01
-        dx = (o * (y - x)) * t
-        x = x + dx
-
-        dy = ((x * (p - z)) - y) * t
-        y = y + dy
-
-        dz = ((x * y) - (B * z)) * t
-        z = z + dz
-
-        if version == "xy" or version == "yx":
-            coords = (int((x*scale)+500+width//2),int((y*scale)+height//2)+height//2)
-        elif version == "zy" or version == "yz":
-            coords = (int((z*scale)+80+width//2),int((y*scale)+height//2)+height//2)
-        elif version == "xz" or version == "zx":
-            coords = (int((x*scale)+500+width//2),int((z*scale)+height//4))
-        else:
-            coords = (int((x*scale)+500+width//2),int((y*scale)+height//2)+height//2)
-
-        pygame.draw.line(screen, colors[colorI], prev_coords, coords)
-        # rainbow >
-        # pygame.draw.line(screen, (np.random.randint(100, 255),np.random.randint(100, 255), np.random.randint(100, 255)), prev_coords, coords)
-        pygame.display.update()
-        # print(x,y,z)
+        for i in range(len(p)):
+            p[i].calc()
+            p[i].draw()
 
 pygame.quit()
 
