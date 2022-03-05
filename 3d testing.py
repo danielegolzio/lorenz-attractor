e21bd51082f443a6aeb8e4328624ed7c07b9ee60
Daniele Golzio <80271815+poonchoi@users.noreply.github.com>
2022-03-05 16:55:55 +0100

    Update 3d testing.py

--- 3d testing.py
+++ 3d testing.py
@@ -1,3 +1,4 @@
+from xml.dom import NotSupportedErr
 from p5 import *
 
 
@@ -5,17 +6,26 @@ def setup():
     size(720, 400)
 
 
+# def draw():
+#     no_stroke()
+#     background(205, 102, 94)
+#     rotate_x(frame_count * 0.02)
+#     rotate_y(frame_count * 0.01)
+#     blinn_phong_material()
+#     cone(200, 300)
+#     locX = mouse_x - width / 2
+#     locY = mouse_y - height / 2
+#     light_specular(0, 0, 255)
+#     point_light(360, 360 * 1.5, 360, locX, locY, 400)
+
+
 def draw():
-    no_stroke()
-    background(205, 102, 94)
+    stroke(114)
+    background(0, 0, 0)
     rotate_x(frame_count * 0.02)
-    rotate_y(frame_count * 0.01)
-    blinn_phong_material()
-    cone(200, 300)
-    locX = mouse_x - width / 2
-    locY = mouse_y - height / 2
-    light_specular(0, 0, 255)
-    point_light(360, 360 * 1.5, 360, locX, locY, 400)
+    rotate_z(frame_count * 0.02)
+    rotate_y(frame_count * 0.02)
+    cone(300, 200)
 
 
 if __name__ == "__main__":
