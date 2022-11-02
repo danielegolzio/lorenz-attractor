e1dff29f083ef10336f8ce55f4dd125d39455b05
Daniele Golzio <danielegolzio@gmail.com>
2022-11-02 22:09:53 +0100

    Update 3D_example.py

--- 3D_example.py
+++ 3D_example.py
@@ -2,25 +2,25 @@ import lorenz.particle as P
 import pygame as pg
 
 def main():
-    fps = 200
+    fps = 100
     screen = pg.display.set_mode(P.dimensions)
     clock = pg.time.Clock()
 
-    population = 100
+    population = 1000
     scale = 15
     point_size = 1
     angle = 0
 
-    p = [P.Particle((0.01+(i*(0.01/population))),[0.1,0,0], scale) for i in range(population)]
+    p = [P.Particle((0.001+(i*(0.001/population))),[0.1,0,0], scale) for i in range(population)]
 
     while True:
-        angle += 0.005
+        angle += 0.01
         screen.fill((0,0,0))
         for i in range(len(p)):
             p[i].calc()
-            p[i].rotateY(angle)
+            #p[i].rotateZ(angle)
             point = p[i].project()
-            pg.draw.circle(screen, (255,255,255), point, point_size)
+            pg.draw.circle(screen, (127,43,124), point, point_size)
 
         [exit() for i in pg.event.get() if i.type == pg.QUIT]
         pg.display.set_caption(f"{round(clock.get_fps())} FPS")
